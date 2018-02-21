0c064ade1b7afddca5a4a653db578fe6cafd64d1
riomontana <forsberglinus@gmail.com>
2018-02-21 15:01:10 +0100

    Flyttat på interface till rätt paket

--- app/src/main/java/com/lfo/partynonsense/MainActivity.java
+++ app/src/main/java/com/lfo/partynonsense/MainActivity.java
@@ -3,8 +3,6 @@ package com.lfo.partynonsense;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-// Testar en pull request / Linus
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
